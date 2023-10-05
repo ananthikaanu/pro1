5d2564468106a59383f075382a514f4821b07ba2
Ananthika <ananthikaspillai@gmail.com>
2023-10-05 11:25:26 +0530

    updated

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import "./Banner.css"
-import axios from 'axios'
-import {API_KEY} from './constant'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../constants/constant'
 
 function Banner() {
   useEffect(() => {
        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then(( response)=>{
-         console.log(response)
+         console.log(response.data)
        })
   },[])
   return (
-    <div className='banner'>
+    <div
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+     className='banner'>
         <div className='content'>
            <h1 className='title'>Movie Name </h1>
              <div className='banner-buttons'>
--- src/components/constants/constant.js
+++
@@ -1,2 +0,0 @@
-export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = "7cbd949035299dc1a1aa894db6ae3afc"
