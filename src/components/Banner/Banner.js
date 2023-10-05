3eaa7e775ce5f13f05da70ea6748895d369f76a8
Ananthika <ananthikaspillai@gmail.com>
2023-10-05 13:57:56 +0530

    changed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,9 +4,12 @@ import axios from '../../axios'
 import {API_KEY,imageUrl} from '../../constants/constant'
 
 function Banner() {
+       const [movie,setMovie] = useState()
   useEffect(() => {
-       axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then(( response)=>{
-         console.log(response.data)
+       axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then(( response)=>{
+         console.log(response.data.results[0])
+         setMovie(response.data.results[0])
+        
        })
   },[])
   return (
@@ -14,12 +17,12 @@ function Banner() {
     style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
      className='banner'>
         <div className='content'>
-           <h1 className='title'>Movie Name </h1>
+           <h1 className='title'>{movie ? movie.title :""}</h1>
              <div className='banner-buttons'>
                <button className='button'> Play </button>
                <button className='button'> My List </button>
              </div>
-             <h1 className='description'>A criminal mastermind who goes by "The Professor" has a plan to pull off the biggest heist in recorded history  to print billions of euros in the Royal Mint of Spain. </h1>
+             <h1 className='description'> {movie ? movie.overview: ""} </h1>
         </div>
         <div className='fade_bottom'></div>
     </div>
